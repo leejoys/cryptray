0425fd3fb0b60e8528af136d9e5234a3ad4a5f89
leejoys <44557202+leejoys@users.noreply.github.com>
2022-01-15 10:59:10 +0200

    view lorca

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module cryptray
 
 go 1.17
 
-require github.com/getlantern/systray v1.1.0
+require (
+	github.com/getlantern/systray v1.1.0
+	github.com/zserge/lorca v0.1.10
+)
 
 require (
 	github.com/getlantern/context v0.0.0-20190109183933-c447772a6520 // indirect
@@ -13,5 +16,6 @@ require (
 	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
+	golang.org/x/net v0.0.0-20200222125558-5a598a2470a0 // indirect
 	golang.org/x/sys v0.0.0-20200515095857-1151b9dac4a9 // indirect
 )
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	_ "embed"
+	"log"
 
 	"github.com/getlantern/systray"
+	"github.com/zserge/lorca"
 )
 
 //go:embed aw.ico
@@ -16,17 +18,30 @@ func main() {
 func onReady() {
 	systray.SetTitle("Sup")
 	systray.SetIcon(ico)
+	v := systray.AddMenuItem("View", "")
+	systray.AddSeparator()
 	q := systray.AddMenuItem("Quit", "")
-	e := systray.AddMenuItem("Exit", "")
 	systray.SetTooltip("Sup")
+
 	for {
 		select {
 		case <-q.ClickedCh:
 			systray.Quit()
-		case <-e.ClickedCh:
-			systray.Quit()
+		case <-v.ClickedCh:
+			view()
 		}
 	}
+}
+
+func view() {
+	// Create UI with basic HTML passed via data URI
+	ui, err := lorca.New("https://leejoys.github.io/", "", 640, 480)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ui.Close()
+	// Wait until UI window is closed
+	<-ui.Done()
 }
 
 func onExit() {
